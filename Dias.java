9caf62079f3c6f834c1c27b739120e52a4d145b9
Carolina Marugan Rubio <54485798+codecamaru@users.noreply.github.com>
2022-02-05 19:25:21 +0100

    Update Dias.java

--- Dias.java
+++ Dias.java
@@ -1,3 +1,21 @@
+/*
+STATEMENT:
+
+Debido a la crisis, el bar de Javier ha notado un descenso de las consumiciones. Además, 
+según dicen en los telediarios, la ley antitabaco le está perjudicando aún más. Como no termina 
+de creerse todo lo que dicen en la televisión, ha decidido hacer un estudio de mercado semanal de 
+su establecimiento. Para ello, ha estado apuntando la caja diaria que se ha realizado en las últimas semanas. 
+Le gustaría saber qué día de la semana se producen el mayor y el menor número de ventas, y si las ventas del 
+domingo superan a la media semanal. De esta manera podrá establecer estrategias de marketing que le permitan 
+recuperar algo de las ganancias perdidas.
+
+Javier abre su bar todos los días menos los Lunes, que utiliza para descansar.
+
+Realiza un programa que ayude a Javier en su cometido. Dada una lista de valores correspondiente a una 
+semana, nuestro programa deberá decirle a Javier el día de la semana que más y menos ha vendido, y si las 
+ventas del domingo superan la media.
+*/
+
 // author: Carolina Marugan
 
 import java.util.Scanner;
